4308b2e9326ff35749021a2fc45fbb7489856e0a
Yuta0526 <soumayuuta526@outlook.jp>
2025-07-26 14:34:09 +0900

    Add files via upload

---
+++ Text4_1.java
@@ -0,0 +1,18 @@
+public class Text4_1 {
+    public static void main(String[] args){
+        int sansu=20;
+        int kokugo=30;
+        int rika=40;
+        int eigo=50;
+        int syakai=80;
+
+        int sum=sansu+kokugo+rika+eigo+syakai;
+
+        int avg=sum/5;
+        System.out.println("合計点："+sum);//自分で点数を変数に入れたら合計と平均を計算してくれる、次回で簡易版があるかも
+        System.out.println("平均点："+avg);
+
+
+    }
+    
+}
