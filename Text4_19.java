783e11479674ba5953360cc1ed3c65751337ecc8
Yuta0526 <soumayuuta526@outlook.jp>
2025-07-31 22:50:18 +0900

    Add files via upload

---
+++ Text4_19.java
@@ -0,0 +1,8 @@
+public class Text4_19 {
+    public static void main(String[]args){
+        int[][]scores={{40,50,60},{80,60,70}};//もう一つ配列を増やした場合に3*3の表が出来る
+        System.out.println(scores.length);/*親配列の長さを表示している */
+        System.out.println(scores[1].length);/*子配列の長さを表示している */
+    }//多次元配列は表の形ではなく入れ子構造
+    
+}
