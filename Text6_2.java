7bde2e334115b83f9dbfc636222a5e6a9c83acb1
Yuta0526 <soumayuuta526@outlook.jp>
2025-08-03 23:54:50 +0900

    Add files via upload

---
+++ Text6_2.java
@@ -0,0 +1,10 @@
+public class Text6_2 {//Text6_4から分割されたメソッド
+    public static int tasu(int a,int b){
+        return a+b;
+    }
+
+    public static int hiku(int a,int b){
+        return a-b;
+    }
+    
+}
---
+++ Text6_4.java
@@ -0,0 +1,10 @@
+public class Text6_4 {
+    public static void main(String[]args){
+        int a=10;
+        int b=2;
+        int total=Text6_2.tasu(a,b);
+        int delta=Text6_2.hiku(a,b);
+        System.out.println("足すと"+total+"、引くと"+delta);
+    }/*メソッドを複数のソースファイルに分けている！！！ */
+    
+}
