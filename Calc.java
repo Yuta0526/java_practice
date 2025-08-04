108c529c9654ee8cd1c964498092fb270f480906
Yuta0526 <soumayuuta526@outlook.jp>
2025-08-04 23:52:43 +0900

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,12 @@
+package java_textbook.CalcLogic;
+
+public class Calc {
+    public static int tasu(int a,int b){
+        return a+b;
+    }
+
+    public static int hiku(int a,int b){
+        return a-b;
+    }
+    
+}
